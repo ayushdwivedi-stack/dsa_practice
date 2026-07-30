a312995e0d7bd426df10d10eaa7439794db78d6c
Ayush Dwivedi <ayushdwivedi8756326270@gmail.com>
2026-07-30 18:26:03 +0530

    Time: 0 ms (100%), Space: 8.2 MB (77.73%) - LeetHub

--- 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
@@ -1,37 +1,13 @@
 class Solution {
 public:
     int minimumPushes(string word) {
-        
-        int sum =0;
-        for(int i = 0 ; i <word.length() ; i++)
+        int totalclick = 0;
 
-        {
-            int click = word.length() % 8 ;
-            if(word.length() <=8)
-            {
-               sum = word.length();
+        for(int i = 0; i < word.length(); i++){
+            int click = (i / 8) + 1;
 
-            }
-            else if(  word.length() >8 && word.length() <=16)
-            {
-                sum =  8 + 2 * (word.length() -8);
-            }
-
-            else if(word.length() >16 && word.length()<= 24)
-            {
-                sum = 24+ 3*(word.length() - 16);
-            }
-            else
-            {
-                sum = 48 + 4*(word.length() -24);
-            }
-                
-            
-
-
+            totalclick += click;
         }
-        return sum ;
-
-        
+        return totalclick;
     }
 };
