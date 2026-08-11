e654b9893bf3eff83a33ca4099774acbbb171053
Ayush Dwivedi <ayushdwivedi8756326270@gmail.com>
2026-08-11 23:44:40 +0530

    Time: 0 ms (100%), Space: 22.6 MB (77.12%) - LeetHub

--- 2996-smallest-missing-integer-greater-than-sequential-prefix-sum/2996-smallest-missing-integer-greater-than-sequential-prefix-sum.cpp
+++ 2996-smallest-missing-integer-greater-than-sequential-prefix-sum/2996-smallest-missing-integer-greater-than-sequential-prefix-sum.cpp
@@ -1,21 +1,29 @@
 class Solution {
 public:
     int missingInteger(vector<int>& nums) {
-     int ans = nums[0];
+        int sum=nums[0];
+        int n=nums.size();
+        for(int i=1;i<n;i++){
+            if(nums[i]==nums[i-1]+1){
+               sum=sum+nums[i];
+                }
+                else
+                break;
+                }
+    
+                int x=sum;
+                while(true){
+                    bool missing=false;
+                    for(int i=0;i<nums.size();i++){
+                        if(nums[i]==x){
+                            missing=true;
+                            break;
+                        }
+                    }
+                    if( !missing)
+                    return x;
 
-for (int i = 1; i < nums.size(); i++) {
-    if (nums[i] == nums[i - 1] + 1)
-        ans = ans + nums[i];
-    else
-        break;
-}
-
-unordered_set<int> s(nums.begin(), nums.end());
-
-while (s.count(ans))
-{
-    ans++;
-}
-return ans;
-}
+                    x++;
+                }
+    }
 };
