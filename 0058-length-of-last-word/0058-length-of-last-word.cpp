c80c7a4736c74e8f02fcca52e294e6e6bd0f82c5
Ayush Dwivedi <ayushdwivedi8756326270@gmail.com>
2026-08-31 16:09:25 +0530

    Time: 0 ms (100%), Space: 8.9 MB (33.72%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int count = 0;
+        int n = s.size();
+         
+        for (int i = n - 1; i >= 0; i--) {
+            
+            if (s[i] == ' ' && count == 0) {
+                continue;
+            }
+            
+            if (s[i] != ' ') {
+                count++;
+            }
+            else {
+                break;
+            }
+        }
+        
+        return count;
+    }
+};
