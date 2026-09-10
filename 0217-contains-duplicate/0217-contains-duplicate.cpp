c332fc70bee08778ed7da1f232c7d814d401257b
Ayush Dwivedi <ayushdwivedi8756326270@gmail.com>
2026-09-11 02:42:48 +0530

    Time: 182 ms (5.33%), Space: 116.2 MB (5.04%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        int n = nums.size();
+        map<int , int>mp;
+         
+        for(int x: nums)
+        {
+            mp[x]++;
+        }
+        for(int i =0; i<n ; i++)
+        {
+            if(mp[nums[i]]>=2)
+            {
+                return true;
+
+            }
+        }
+        return false;
+        
+    }
+};
